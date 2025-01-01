6d2b35d3dfe405dfa7897b2125263d7c1adf2bd6
Vyankatesh Salunkhe <venky.salunkhe@gmail.com>
2025-01-01 09:16:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return "Welcome to 2025 FORK DEMO! Version 2\n We're so glad you're here to celebrate the new year!"
+    return "Welcome to 2025 jenkins DEMO! Version 2\n We're so glad you're here to celebrate the new year!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
